98193682f834e8338c460bcdf594bdea66214a45
Thalia <thaliatelopoulou@gmail.com>
2023-06-21 00:08:08 +0300

    exercise 1 completed

---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Hello(){
+   return (
+    <h1>Hello</h1>
+   )
+}
+
+
---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Message(){
+    return (
+        <p>It's time to learn React!!!</p>
+    )
+}
