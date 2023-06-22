2b9d14ab5a1308ad665ecae14102e71d908b1512
Thalia <thaliatelopoulou@gmail.com>
2023-06-22 12:40:21 +0300

    exercise 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Hello } from "./components/Hello";
 import { Message } from "./components/Message";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <>
-      <Hello/>
-      <Message/>
+      {/* <Hello/>
+      <Message/> */}
+      <Welcome name = "Ted"/>
    </>
   );
 }
