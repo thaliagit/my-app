e9743fa975d934cd0da4fbde6c8fd486ccf30ddd
Thalia <thaliatelopoulou@gmail.com>
2023-06-26 22:20:50 +0300

    exercise 7 completed

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,7 @@ class Welcome extends React.Component {
     return (
       <>
         <p>Welcome, {this.props.name}!</p>
-       <Age age = {this.props.age}/>
+       {this.props.age > 18 && <Age age = {this.props.age}/>}
       </>
     );
   }
