c6e0622ff5d0751b59f8daf02d7c48ef2b87814b
Thalia <thaliatelopoulou@gmail.com>
2023-06-28 12:00:19 +0300

    exercise 13 completed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,24 +5,30 @@ class Counter extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-          count : 0
+          count : this.props.initialCount
         };
 
         setInterval(()=>{
           this.setState(prevState => ({
-              count: prevState.count += 1
+              count: prevState.count + this.props.initialIncrement
           }))
-      }, 1000)
+      }, this.props.initialIntervalIncr)
   }
 
   
   render() {
     return (
       <>
-        {this.state.count < 10 && <h1>{this.state.count}</h1>}
+        {this.state.count < 150 && <h1>{this.state.count}</h1>}
       </>
     );
   }
+
 }
 
+Counter.defaultProps = {
+  initialCount : 10,
+  initialIncrement : 5,
+  initialIntervalIncr: 1500
+}
 export default Counter;
