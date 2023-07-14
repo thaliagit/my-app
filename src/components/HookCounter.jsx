4e882618e50177c35bd6938f7fe488808744c5bc
Thalia <thaliatelopoulou@gmail.com>
2023-07-14 12:26:49 +0300

    exercise 36 updated decrement functinality

--- src/components/HookCounter.jsx
+++ src/components/HookCounter.jsx
@@ -33,6 +33,7 @@ export default function HookCounter({ initialValue = 0 }) {
     <div>
         <h2>Counter: {counter}</h2>
         <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
         <button onClick={onReset}>Reset</button>
     </div>
   )
