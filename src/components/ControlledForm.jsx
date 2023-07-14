cb62fbc04647a68da8ccfdd1df46bc578a067c9a
Thalia <thaliatelopoulou@gmail.com>
2023-07-14 20:47:25 +0300

    exercise-37 completed

--- src/components/ControlledForm.jsx
+++ src/components/ControlledForm.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function useForm(){
-
+  const [input, setInput] = useState('')
+  function handleInput(e){
+    setInput(e.target.value)
+  }
+  //console.log(input)
+  return {
+    input : input,
+    handleInpVal : handleInput
+  }
 }
 
 function ControlledForm() {
+  const {input, handleInpVal} = useForm()
   return (
     <>
         <form> 
             <label htmlFor="username">Username: </label>
-            <input type="text" name="username" />
+            <input type="text" name="username" onChange={handleInpVal} />
             <label htmlFor="password">Password: </label>
-            <input type="password" name="lastName" />
+            <input type="password" name="lastName" onChange={handleInpVal}/>
         </form>
     </>
   )
