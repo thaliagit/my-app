6f372e06e06c7d2d61e03a35b6f867159a9c23b6
Thalia <thaliatelopoulou@gmail.com>
2023-07-16 10:01:31 +0300

    exercise 42 incomplete

--- src/components/HookCounter.jsx
+++ src/components/HookCounter.jsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react'
 
-function useCounter(initialValue = 0){
-  const [counter, setCounter] = useState(initialValue)
+// function useCounter(initialValue = 0){
+//   const [counter, setCounter] = useState(initialValue)
 
-  function handleCounterIncr(){
-    setCounter((c) => c + 1)
-  }
+//   function handleCounterIncr(){
+//     setCounter((c) => c + 1)
+//   }
 
-  function handleCounterDecrement(){
-    setCounter((c) => c - 1)
-  }
+//   function handleCounterDecrement(){
+//     setCounter((c) => c - 1)
+//   }
 
-  function handleCounterReset(){
-    setCounter(initialValue)
-  }
+//   function handleCounterReset(){
+//     setCounter(initialValue)
+//   }
 
-  return { 
-    counter: counter,
-    onIncrement: handleCounterIncr,
-    onDecrement: handleCounterDecrement,
-    onReset: handleCounterReset
-  }
+//   return { 
+//     counter: counter,
+//     onIncrement: handleCounterIncr,
+//     onDecrement: handleCounterDecrement,
+//     onReset: handleCounterReset
+//   }
 
-}
+// }
 
 export default function HookCounter({ initialValue = 0 }) {
-  const {counter, onIncrement, onDecrement, onReset} = useCounter(initialValue)
+  // const {counter, onIncrement, onDecrement, onReset} = useCounter(initialValue)
 
-  
+  const [counter, setCounter] = useState(initialValue)
+  function handleIncrement(){
+    setCounter(c => c + 1)
+  }
 
     return (
     <div>
         <h2>Counter: {counter}</h2>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
-        <button onClick={onReset}>Reset</button>
+        <button onClick={handleIncrement}>Increment</button>
+        {/* <button onClick={onDecrement}>Decrement</button>
+        <button onClick={onReset}>Reset</button> */}
     </div>
   )
 }
